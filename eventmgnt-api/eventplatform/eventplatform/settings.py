eea9c9894ba8c8eb83aa292471c88f2093b550b9
rimilmathew <rimilmathew.rimil@gmail.com>
2026-06-16 19:09:01 +0530

    config change

--- eventmgnt-api/eventplatform/eventplatform/settings.py
+++ eventmgnt-api/eventplatform/eventplatform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#72mwai6#fgj+21%1#=#4$*@iafi7d&f8jidk))1r)m2-&bhr8'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-#72mwai6#fgj+21%1#=#4$*@iafi7d&j8fjdk))1r)m2-&bhr8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -138,3 +144,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- eventplatform-ui/src/services/api.js
+++ eventplatform-ui/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const api = axios.create({baseURL: "http://127.0.0.1:8000",})
+const api = axios.create({baseURL: process.env.REACT_APP_API_URL,})
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token")
